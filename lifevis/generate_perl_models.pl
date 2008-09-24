b68e54665d58d06d16c87f71bf15df63c03cd9ee
mithaldu@yahoo.de <mithaldu@yahoo.de@ab7e0b5b-ab3f-0410-b06b-8d30d230cac2>
2008-09-24 18:38:32 +0000

    lifevis: The model generation script now creates x 90°-rotated models of each input based on the number that's last in the filename. All model draw functions are now stored in a hash.
    git-svn-id: http://dwarvis.googlecode.com/svn/trunk@114 ab7e0b5b-ab3f-0410-b06b-8d30d230cac2

--- lifevis/generate_perl_models.pl
+++ lifevis/generate_perl_models.pl
@@ -3,6 +3,7 @@ use 5.010;
 use strict;
 
 use Math::Vec qw(:terse);
+use Math::Trig ':pi';
 
 my %side = (
                 "0,0,1" => "south",
@@ -13,22 +14,29 @@ my %side = (
                 "0,0,-1" => "north",
                 "0.707106781186547,0.707106781186547,0" => "top right",
                 "0,0.707106781186547,0.707106781186547" => "top south",
+                "-0.707106781186547,0.707106781186547,0" => "top left",
+                "0,0.707106781186547,-0.707106781186547" => "top north",
 
             );
 
 
 sub generateModel {
     my ($input) = @_;
+    
     my @raw_data;
     
     my @vertices;
     my @faces;
     my @uvs;
     my @normals;
+    my $model = '';
     
     open my $DAT, "<", "models/$input.obj" or die( "horribly: ".$! );
     @raw_data=<$DAT>;
     close $DAT;
+    
+    my $rotations = 1;
+    $rotations = $1 if ( $input =~ s/(\d)$// );
     
     for my $line (@raw_data) {
         if ( $line =~ m/^v (.*?) (.*?) (.*?)$/ ){
@@ -49,62 +57,71 @@ sub generateModel {
             $uvs[$#uvs][1] = ($2+0);
         }
     }
-    for my $id (@faces) {
-        my $normal_is_new = 1;
-        my $existing_normal;
-        
-        my $normal = calcFaceNormal(
-                        $vertices[$id->{verts}[0]{coords}],
-                        $vertices[$id->{verts}[1]{coords}],
-                        $vertices[$id->{verts}[2]{coords}] );
-        
-        if ($#normals != -1) {
-            for my $nid ( 0..$#normals) {
-                if ( $normal->[0] == $normals[$nid][0] && $normal->[1] == $normals[$nid][1] && $normal->[2] == $normals[$nid][2] ) {
-                    $existing_normal = $nid;
-                    $normal_is_new = 0;
-                    last;
+    
+    for my $rotation (1..$rotations) {
+        undef @normals;
+        for my $id (@faces) {
+            my $normal_is_new = 1;
+            my $existing_normal;
+            
+            my $normal = calcFaceNormal(
+                            $vertices[$id->{verts}[0]{coords}],
+                            $vertices[$id->{verts}[1]{coords}],
+                            $vertices[$id->{verts}[2]{coords}] );
+            
+            if ($#normals != -1) {
+                for my $nid ( 0..$#normals) {
+                    if ( $normal->[0] == $normals[$nid][0] && $normal->[1] == $normals[$nid][1] && $normal->[2] == $normals[$nid][2] ) {
+                        $existing_normal = $nid;
+                        $normal_is_new = 0;
+                        last;
+                    }
                 }
             }
+            
+            if ($normal_is_new) {
+                $normals[$#normals+1] = calcFaceNormal(
+                                                       $vertices[$id->{verts}[0]{coords}],
+                                                       $vertices[$id->{verts}[1]{coords}],
+                                                       $vertices[$id->{verts}[2]{coords}] );
+                $id->{normal} = $#normals;
+            }
+            else {
+                $id->{normal} = $existing_normal;
+            }
         }
         
-        if ($normal_is_new) {
-            $normals[$#normals+1] = calcFaceNormal(
-                                                   $vertices[$id->{verts}[0]{coords}],
-                                                   $vertices[$id->{verts}[1]{coords}],
-                                                   $vertices[$id->{verts}[2]{coords}] );
-            $id->{normal} = $#normals;
-        }
-        else {
-            $id->{normal} = $existing_normal;
-        }
-    }
+        $rotation = '' if ( $rotations == 1 );
+        $model .= "\n\n\$draw{$input$rotation} = sub {
+        my (\$x, \$y, \$z, \$s) = \@_;
+        my \$brightness = \$y/(\$zcount-15);
+        glColor3f(\$brightness, \$brightness, \$brightness);";
     
-    my $model = "sub draw$input {
-    my (\$x, \$y, \$z, \$s) = \@_;
-    my \$brightness = \$y/(\$zcount-15);
-    glColor3f(\$brightness, \$brightness, \$brightness);
-";
-
-    my @old = (999,999,999);
-    for my $nid ( 0..$#normals) {
-        for my $fid ( 0..$#faces) {
-            next if ( $faces[$fid]{normal} != $nid );
+        my @old = (999,999,999);
+        for my $nid ( 0..$#normals) {
+            for my $fid ( 0..$#faces) {
+                next if ( $faces[$fid]{normal} != $nid );
+                
+                if( $old[0] != $normals[$nid][0] || $old[1] != $normals[$nid][1] || $old[2] != $normals[$nid][2] ) {
+                    my $vec = "$normals[$nid][0],$normals[$nid][1],$normals[$nid][2]";
+                    $model .= "\n\n    glNormal3f( $vec );";
+                    $model .= "# $side{$vec} face" if defined $side{$vec};
+                }
+                for my $vid ( 0..2) {
+                    my $uv = $faces[$fid]{verts}[$vid]{uv};
+                    my $vert = $faces[$fid]{verts}[$vid]{coords};
+                    $model .= "\n    glTexCoord2f($uvs[$uv][0],$uvs[$uv][1]); glVertex3f($vertices[$vert][0]+\$x,$vertices[$vert][1]+\$y,$vertices[$vert][2]+\$z);";
+                }
+                @old = ( $normals[$nid][0],$normals[$nid][1],$normals[$nid][2]);
+            }
+        }
+        
+        $model .= "\n};";
             
-            if( $old[0] != $normals[$nid][0] || $old[1] != $normals[$nid][1] || $old[2] != $normals[$nid][2] ) {
-                my $vec = "$normals[$nid][0],$normals[$nid][1],$normals[$nid][2]";
-                $model .= "\n    glNormal3f( $vec ); # $side{$vec} face\n";
-            }
-            for my $vid ( 0..2) {
-                my $uv = $faces[$fid]{verts}[$vid]{uv};
-                my $vert = $faces[$fid]{verts}[$vid]{coords};
-                $model .= "    glTexCoord2f($uvs[$uv][0],$uvs[$uv][1]); glVertex3f($vertices[$vert][0]+\$x,$vertices[$vert][1]+\$y,$vertices[$vert][2]+\$z);\n";
-            }
-            @old = ( $normals[$nid][0],$normals[$nid][1],$normals[$nid][2]);
+        for my $vid (0..$#vertices) {
+            ($vertices[$vid][0],$vertices[$vid][2]) = rotate_vector($vertices[$vid][0],$vertices[$vid][2]);
         }
     }
-    
-    $model .= "}";
     
     return $model;
 }
@@ -143,7 +160,11 @@ BEGIN {
 
 our \$zcount;
 
-$code";
+our \%draw;
+
+$code
+
+";
 close $OUT;    
 
 
@@ -162,5 +183,19 @@ sub calcFaceNormal {
     
     my $normal = $cross/$length;
     
+    for my $val (@{$normal}) {
+        $val = 0 if( $val < 0.0001 && $val > -0.0001);
+        $val = 0.707106781186547 if ( $val > 0.707106 && $val < 0.707107 );
+        $val = -0.707106781186547 if ( $val < -0.707106 && $val > -0.707107 );
+    }
+    
     return $normal;
+}
+
+
+sub rotate_vector {
+    my ($x,$z) = @_;
+    my $xnew = cos(pip2)*($x) - sin(pip2)*$z;
+    my $znew = sin(pip2)*($x) + cos(pip2)*$z;
+    return ($xnew,$znew);
 }
